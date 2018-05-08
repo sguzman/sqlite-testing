bf7d131f828781082b8b61915606b4839547ece7
Salvador Guzman <guzmansalv@gmail.com>
2018-05-08 09:52:28 -0700

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sqlite3.h>
+#include "crow_all.h"
 
 using std::cout;
 using std::endl;
@@ -19,6 +20,10 @@ static inline int callback(void *, int argc, char** argv, char** azColName) noex
   return 0;
 }
 
+static inline const char * const resp(const std::string& str) noexcept {
+  return str.c_str();
+}
+
 int main() noexcept {
   sqlite3* db;
   int rc{sqlite3_open("../item.db", &db)};
@@ -28,6 +33,19 @@ int main() noexcept {
     } else {
       cout << "Open database successfully" << endl;
     }
+  }
+
+  {
+    crow::SimpleApp app{};
+
+    CROW_ROUTE(app, "/<string>")(resp);
+
+    app
+        .port(8080)
+        .bindaddr("0.0.0.0")
+        .concurrency(8)
+        .multithreaded()
+        .run();
   }
 
   {
